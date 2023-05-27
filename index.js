515b86da9dbee76b3fc056ad00fc600ceb45380f
Rachael <rachael.brady@theanalyticalmoose.com>
2023-05-26 19:14:48 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Your code here
+// Get the dodger element
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    left--;
+    dodger.style.left = `${left}px`;
+  }
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
+  const gameWidth = document.getElementById("game").offsetWidth;
+  const dodgerWidth = dodger.offsetWidth;
+  const rightEdge = left + dodgerWidth;
+
+  if (rightEdge < gameWidth) {
+    left++;
+    dodger.style.left = `${left}px`;
+  }
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
